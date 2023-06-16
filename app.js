a406d49095a8bfeff9936ac2819d1c8718576174
dhruvmehra <dhruv.mehra@live.com>
2023-06-16 18:51:40 +0530

    modified for prod

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+// var logger = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const authenticateUser = require("./middleware/authentication");
@@ -31,7 +31,7 @@ app.set("view engine", "jade");
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -55,8 +55,7 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  res.status(err.status || 500).json({ error: err.message });
 });
 
 module.exports = app;
